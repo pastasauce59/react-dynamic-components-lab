11af2ac88e3475a79e1e143bc7950c52f9c7ab8c
Michael Primavera <pastasauce59@gmail.com>
2021-04-26 17:16:15 -0400

    commit tree fe276435c227b4bc3ede538ed2f94e6ff794214e

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -10,8 +10,12 @@ export default class ColorBox extends Component {
 
   render() {
     return (
-      <div className="color-box" style={{opacity: null /*replace null with the value*/}}>
-        {/* your conditional code here! */}
+      <div className="color-box" style={{opacity: this.props.opacity /*replace null with the value*/}}>
+        { this.props.opacity >= 0.2,
+          render() {
+            const newValue = this.props.opacity - 0.1;
+          }
+        }
       </div>
     )
   }
